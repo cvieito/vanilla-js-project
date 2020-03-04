55a96ea7ae08641dfe379237d852793369885d7d
catesv <catesv@users.noreply.github.com>
2020-03-04 18:34:50 +0000

    Updated next hours forecast

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ function displayForecast(response) {
   for (let index = 0; index < 6; index++) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
-    <div class="col-2">
+    <div class="col">
       <h4 class="hours">
       ${formatHours(forecast.dt * 1000)}
       </h4>
